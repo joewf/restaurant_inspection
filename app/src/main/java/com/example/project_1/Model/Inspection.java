92c871eafbfe8a38f03cac2b0f3fa71afae083c0
jiqianp <jiqianp@sfu.ca>
2020-10-29 16:37:16 -0700

    Add fields for Inspection class

--- app/src/main/java/com/example/project_1/Model/Inspection.java
+++ app/src/main/java/com/example/project_1/Model/Inspection.java
@@ -1,4 +1,17 @@
 package com.example.project_1.Model;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
 public class Inspection {
+    InspectionType type;
+    HazardRating hazardRating;
+    List<Violation> violationLump = new ArrayList<>();
+    String trackingNumber;
+    int numCritical;
+    int numNonCritical;
+    int hazardColor;
+    int hazardIcon;
+    GregorianCalendar date;
 }
---
+++ app/src/main/java/com/example/project_1/Model/Violation.java
@@ -0,0 +1,5 @@
+package com.example.project_1.Model;
+
+public class Violation {
+
+}
