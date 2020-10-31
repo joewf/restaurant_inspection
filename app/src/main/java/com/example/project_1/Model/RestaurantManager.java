0456ee20135b89959fb7af2249918c76fcc946e8
Jiahua Wu <jwa318@sfu.ca>
2020-10-30 20:03:30 -0700

    Restaurant manager class created

--- app/src/main/java/com/example/project_1/Model/RestaurantManager.java
+++ app/src/main/java/com/example/project_1/Model/RestaurantManager.java
@@ -1,4 +1,48 @@
 package com.example.project_1.Model;
 
+import androidx.annotation.NonNull;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class RestaurantManager {
+    private List<Restaurant> restaurants = new ArrayList<>();
+    private static RestaurantManager instance;
+
+    // Return restaurant for MyListAdapter
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+
+    /*
+        Singleton support
+     */
+    private RestaurantManager() {
+        // To prevent anyone else from instantiating
+    }
+
+    public static RestaurantManager getInstance() {
+        if (instance == null) {
+            instance = new RestaurantManager();
+        }
+        return instance;
+    }
+
+    public void add(Restaurant restaurant) {
+        restaurants.add(restaurant);
+    }
+
+    public void remove(Restaurant restaurant) {
+        restaurants.remove(restaurant);
+    }
+
+    public Restaurant get(int index) {
+        return restaurants.get(index);
+    }
+
+    @Override
+    public Iterator<Restaurant> iterator() {
+        return restaurants.iterator();
+    }
 }
