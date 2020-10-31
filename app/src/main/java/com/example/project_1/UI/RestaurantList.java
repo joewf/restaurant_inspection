bbe4ea0f217519f8e7b6dfda4e72b1718df00f91
Jiahua Wu <jwa318@sfu.ca>
2020-10-30 20:54:14 -0700

    Testing RestaurantList

--- app/src/main/java/com/example/project_1/UI/RestaurantList.java
+++ app/src/main/java/com/example/project_1/UI/RestaurantList.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.project_1.Model.Restaurant;
+import com.example.project_1.Model.RestaurantManager;
 import com.example.project_1.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RestaurantList extends AppCompatActivity {
+
+    private RestaurantManager restaurantManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_restaurant_list);
+
+        restaurantManager.getInstance();
+
+        readRestaurantData();
+    }
+
+    private void readRestaurantData() {
+        Restaurant sample;
     }
 }
