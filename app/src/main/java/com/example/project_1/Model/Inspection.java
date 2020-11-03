5b29d4dec7985c411f0817151838e9bf258dbeb3
Jiahua Wu <jwa318@sfu.ca>
2020-11-03 03:30:39 -0800

    Couldn't set the type and hazard

--- app/src/main/java/com/example/project_1/Model/Inspection.java
+++ app/src/main/java/com/example/project_1/Model/Inspection.java
@@ -13,33 +13,47 @@ import java.util.List;
 public class Inspection {
     private List<Violation> violationLump = new ArrayList<>();
 
-    private GregorianCalendar date;
+    //private GregorianCalendar date;
     private InspectionType type;
     private HazardRating hazardRating;
     private String trackingNumber;
+    private String date;
     private int numCritical;
     private int numNonCritical;
     private int hazardColor;
     private int hazardIcon;
+    private List<Violation> violations;
 
-    public Inspection(int year, int month, int dayOfMonth, InspectionType type, HazardRating hazardRating, String trackingNumber, int numCritical, int numNonCritical, int hazardColor, int hazardIcon) {
-        setDate(year, month, dayOfMonth);
-        this.type = type;
-        this.hazardRating = hazardRating;
+
+    //int year, int month, int dayOfMonth,
+    //int hazardColor, int hazardIcon
+
+    public Inspection(String trackingNumber, String date, InspectionType type, int numCritical,
+                      int numNonCritical, HazardRating hazardRating, List<Violation> violations) {
+
+        //setDate(year, month, dayOfMonth);
         this.trackingNumber = trackingNumber;
+        this.date = date;
+        this.type = type;
         this.numCritical = numCritical;
         this.numNonCritical = numNonCritical;
-        this.hazardColor = hazardColor;
-        this.hazardIcon = hazardIcon;
+        this.hazardRating = hazardRating;
+        this.violations = violations;
+        //this.hazardColor = hazardColor;
+        //this.hazardIcon = hazardIcon;
     }
 
-    public void setDate(int year, int month, int dayOfMonth) {
+    /*public void setDate(int year, int month, int dayOfMonth) {
         date = new GregorianCalendar(year, month, dayOfMonth);
     }
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     public String getDateString() {
         return Month.of(date.get(Calendar.MONTH)) + " " + date.get(Calendar.DAY_OF_MONTH) + ", " + date.get(Calendar.YEAR);
+    }*/
+
+    public List<Violation> getViolations() {
+        return violations;
     }
 
     public InspectionType getType() {
@@ -70,21 +84,17 @@ public class Inspection {
         return hazardIcon;
     }
 
-    public List<Violation> getViolationLump() {
-        return violationLump;
+    public String getDate() {
+        return date;
     }
 
-    public void setViolationLump(List<Violation> violationLump) {
-        this.violationLump = violationLump;
-    }
-
-    public GregorianCalendar getDate() {
+    /*public GregorianCalendar getDate() {
         return date;
     }
 
     public void setDate(GregorianCalendar date) {
         this.date = date;
-    }
+    }*/
 
     public void setType(InspectionType type) {
         this.type = type;
@@ -98,12 +108,20 @@ public class Inspection {
         this.trackingNumber = trackingNumber;
     }
 
+    public void setDate(String date) {
+        this.date = date;
+    }
+
     public void setNumCritical(int numCritical) {
         this.numCritical = numCritical;
     }
 
     public void setNumNonCritical(int numNonCritical) {
         this.numNonCritical = numNonCritical;
+    }
+
+    public void setViolations(List<Violation> violations) {
+        this.violations = violations;
     }
 
     public void setHazardColor(int hazardColor) {
--- app/src/main/java/com/example/project_1/UI/InspectionReport.java
+++ app/src/main/java/com/example/project_1/UI/InspectionReport.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.example.project_1.Model.HazardRating;
 import com.example.project_1.Model.Inspection;
+import com.example.project_1.Model.InspectionType;
 import com.example.project_1.R;
 
 import java.io.BufferedReader;
@@ -18,42 +20,65 @@ import java.util.List;
 
 public class InspectionReport extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inspection_report);
+
+        getinspectiondata();
     }
 
-    private List<Inspection> Inspectionlist = new ArrayList<>();
+    private List<Inspection> inspectionList = new ArrayList<>();
 
     private void getinspectiondata() {
 
         InputStream is = getResources().openRawResource(R.raw.inspectionreports_itr1);
         BufferedReader reader = new BufferedReader(
                 new InputStreamReader(is, Charset.forName("UTF-8")));
-        String line = "";
-        while (true) {
-            try {
-                //Step over headers
-                reader.readLine();
-                while (!((line = reader.readLine()) != null)) {
-                    Log.d("InspectionReport", "line: " + line);
-                    //split upon commas
-                    String[] tokens = line.split(",");
-                    //reading in data
-                    Inspection inspectdata = new Inspection();
-                    inspectdata.setTrackingNumber(tokens[0]);
-                    inspectdata.setDate(tokens[1]);
-                    inspectdata.setType(tokens[2]);
-                    inspectdata.setNumCritical(tokens[3]);
-                    inspectdata.setNumNonCritical(tokens[4]);
-                    inspectdata.setHazardRating(tokens[5]);
-                    inspectdata.setViolationLump(tokens[6]);
-                    Inspectionlist.add(inspectdata);
 
-                    Log.d("InspectionReport", "created" + inspectdata);
+        String line = "";
+        try {
+            //Step over headers
+            reader.readLine();
+
+            while ( ((line = reader.readLine()) != null) ) {
+
+                Log.d("InspectionReport", "line: " + line);
+                //split upon commas
+                String[] tokens = line.split(",");
+                //reading in data
+                Inspection inspectData = new Inspection();
+                inspectData.setTrackingNumber(tokens[0]);
+                inspectData.setDate(tokens[1]);
+
+                // Couldn't get the type and hazard
+                    if (line == "Routine") {
+                        inspectData.setType(tokens[2]) = InspectionType.ROUTINE;
+
+                    }
+                    if (line == "Follow-Up"){
+                        inspectData.setType(tokens[2]) = InspectionType.FOLLOW_UP;
+                    }
+                inspectData.setNumCritical(Integer.parseInt(tokens[3]));
+                inspectData.setNumNonCritical(Integer.parseInt(tokens[4]));
+                if(line == "Low") {
+                    inspectData.setHazardRating(tokens[5]) = HazardRating.LOW;
                 }
-            } catch (IOException e) {
+                if(line == "Moderate") {
+                    inspectData.setHazardRating(tokens[5]) = HazardRating.MODERATE;
+                }
+                if(line == "High") {
+                    inspectData.setHazardRating(tokens[5]) = HazardRating.HIGH;
+                }
+
+
+                inspectionList.add(inspectData);
+
+                Log.d("InspectionReport", "created" + inspectData);
+            }
+        } catch (IOException e) {
                 Log.wtf("InspectionReport", "error reading file on line", e);
                 e.printStackTrace();
             }
@@ -62,6 +87,6 @@ public class InspectionReport extends AppCompatActivity {
 
 
     }
-}
+
 
 
