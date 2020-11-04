ba3b0b86db12cd9f476e4932279aefcc39507b1d
jiqianp <jiqianp@sfu.ca>
2020-11-04 01:30:26 -0800

    Add Violation class

--- app/src/main/java/com/example/project_1/Model/Violation.java
+++ app/src/main/java/com/example/project_1/Model/Violation.java
@@ -1,5 +1,14 @@
 package com.example.project_1.Model;
 
 public class Violation {
+    private String description;
+    private ViolationNature nature;
+    private ViolationSeverity severity;
+    private int code;
 
+    public Violation(String description, ViolationSeverity severity, int code) {
+        this.description = description;
+        this.severity = severity;
+        this.code = code;
+    }
 }
---
+++ app/src/main/java/com/example/project_1/Model/ViolationSeverity.java
@@ -0,0 +1,5 @@
+package com.example.project_1.Model;
+
+public enum ViolationSeverity {
+    CRITICAL, NON_CRITICAL
+}
