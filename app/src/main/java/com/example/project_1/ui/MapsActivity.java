3e3c9219f7964287162c858f1c3fc5a50c1e6ee0
Jiahua Wu <jwa318@sfu.ca>
2020-11-14 01:09:57 -0800

    Starting map

--- app/src/main/java/com/example/project_1/ui/MapsActivity.java
+++ app/src/main/java/com/example/project_1/ui/MapsActivity.java
@@ -1,8 +1,16 @@
 package com.example.project_1.ui;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.project_1.R;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,18 +22,77 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST = 101;
+
     private GoogleMap mMap;
+    private static final String TAG = "Map activity";
+    private boolean mLocationPermissionGranted = false;
+    private Location currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        getLocationPermission();
+    }
+
+    private void getLocationPermission() {
+        Log.d(TAG, "Getting permission location");
+        String[] permissions = {FINE_LOCATION, COARSE_LOCATION};
+        int checkFineLocation = ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION);
+        int checkCoarseLocation = ContextCompat.checkSelfPermission(this.getApplicationContext(), COARSE_LOCATION);
+
+        if ( checkFineLocation == PackageManager.PERMISSION_GRANTED) {
+            if (checkCoarseLocation == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionGranted = true;
+                initiateMap();
+            }
+        }
+        else {
+            ActivityCompat.requestPermissions(this,
+                    permissions, LOCATION_PERMISSION_REQUEST);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG,"onRequestPermissionResult: called");
+        mLocationPermissionGranted = false;
+
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST:{
+                if (grantResults.length > 0 ) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionGranted = false;
+                            Log.d(TAG, "onRequestPermissionsResult : permission failed");
+                            return;
+                        }
+                    }
+                    Log.d(TAG, "onRequestPermissionResult: permission granted");
+                    mLocationPermissionGranted = true;
+                    // Initiate our map
+                    initiateMap();
+                }
+            }
+        }
+    }
+
+    private void initiateMap() {
+
+        Log.d(TAG, "Initiating map");
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
+    private void getDeviceLocation() {
+
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
